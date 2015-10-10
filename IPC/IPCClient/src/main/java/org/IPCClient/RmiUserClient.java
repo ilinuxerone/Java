44a9ffcd6e6410eee3c9560626d83d6381e0c130
zhzhh <zhihongzhang_hi@163.com>
2015-10-10 22:24:00 +0800

    httpclient usage of post and get method

--- IPC/IPCClient/src/main/java/org/IPCClient/RmiUserClient.java
+++ IPC/IPCClient/src/main/java/org/IPCClient/RmiUserClient.java
@@ -3,6 +3,9 @@ package org.IPCClient;
 import java.rmi.Naming;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 import org.Rmi.domain.UserDO;
 import org.Rmi.service.UserService;
 
@@ -11,7 +14,13 @@ public class RmiUserClient{
 	public static void main(String[] args) {
 		// 调用远程对象，注意RMI路径与接口必须与服务器配置一致
 		try {
-			UserService personService = (UserService) Naming.lookup("rmi://127.0.0.1:8888/PersonService");
+			//1.RMI方法 
+			UserService personService = (UserService) Naming.lookup("rmi://127.0.0.1:8888/PersonServiceDemo");
+			//
+			String url = "rmi://127.0.0.1:8888/PersonServiceDemo";
+			//2.JNDI方式
+			//Context context = new InitialContext();
+			//personService = (UserService)context.lookup(url);
 			List<UserDO> personList = personService.GetList();
 			for (UserDO person : personList) {
 				System.out.println("ID:" + person.getId() + " Age:"
--- IPC/Rmi/src/main/java/org/Rmi/service/RmiUserServer.java
+++ IPC/Rmi/src/main/java/org/Rmi/service/RmiUserServer.java
@@ -4,6 +4,10 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
 public class RmiUserServer {
 
@@ -11,13 +15,17 @@ public class RmiUserServer {
 		try {
 			UserService personService=new UserServiceImpl();
 			//注册通信端口
-			LocateRegistry.createRegistry(8888);
+			Registry registry = LocateRegistry.createRegistry(8888);
 			//注册通信路径
-			Naming.rebind("rmi://127.0.0.1:8888/PersonService", personService);
+			//1.Naming:完整的URI方式的服务名称 2.Registry 3.JNDI:需要指定完整的URI方式的服务名称
+			Naming.rebind("rmi://127.0.0.1:8888/PersonServiceDemo", personService);
+			//registry.bind("PersonServiceDemo", personService);
+			//Context nameContext = new InitialContext();
+			//nameContext.rebind("rmi://127.0.0.1:8888/PersonServiceDemo", personService);
 			System.out.println("server is ready");
 		} catch (RemoteException e) {
 			e.printStackTrace();
-		} catch (MalformedURLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
