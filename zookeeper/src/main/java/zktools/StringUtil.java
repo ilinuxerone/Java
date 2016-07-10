4fbec66ba115a995a231cb3f63e4e4fda4613626
zhzhh <zhihongzhang_hi@163.com>
2016-07-10 22:10:55 +0800

    zookeeper client tool

---
+++ zookeeper/src/main/java/zktools/StringUtil.java
@@ -0,0 +1,44 @@
+package zktools;
+
+public class StringUtil {  
+	  
+    public static String strLeft(final String text, String subtext) {  
+        if(!hasText(text) || !hasText(subtext)) {  
+            return "";  
+        }  
+          
+        int find = text.indexOf(subtext);  
+        return (find!=-1) ? text.substring(0, find) : "";  
+    }  
+      
+    public static String strRight(final String text, String subtext) {  
+        if(!hasText(text) || !hasText(subtext)) {  
+            return "";  
+        }  
+          
+        int find = text.indexOf(subtext);  
+        return (find!=-1) ? text.substring(find+1) : "";  
+    }  
+  
+    public static String strLeftBack(final String text, String subtext) {  
+        if(!hasText(text) || !hasText(subtext)) {  
+            return "";  
+        }  
+          
+        int find = text.lastIndexOf(subtext);  
+        return (find!=-1) ? text.substring(0, find) : "";  
+    }  
+      
+    public static String strRightBack(final String text, String subtext) {  
+        if(!hasText(text) || !hasText(subtext)) {  
+            return "";  
+        }  
+          
+        int find = text.lastIndexOf(subtext);  
+        return (find!=-1) ? text.substring(find+1) : "";  
+    }  
+      
+    private static boolean hasText(String text) {  
+        return (text!=null) && (!"".equals(text));  
+    }  
+}  
